866e01cdc767d969f1b4a89f05afafc0d5bb5ab0
HarryDo29 <anhdonguyennhi@gmail.com>
2025-06-26 01:04:54 +0700

    Add notification integration for new account types
    - Enhanced `createAdminController`, `createManagerController`, `createStaffController`, `createConsultantController`, and `createCustomerController` to send notifications upon successful account creation.
    - Introduced new notification types in `type_noti.enum.ts` to support these actions, improving user feedback and engagement.

--- src/controllers/admin.controller.ts
+++ src/controllers/admin.controller.ts
@@ -1,10 +1,20 @@
 import { NextFunction, Request, Response } from 'express'
 import adminService from '../services/admin.service.js'
 import { ADMIN_MESSAGES } from '../constants/message.js'
+import notificationService from '~/services/notification.service.js'
+import { TypeNoti } from '~/enum/type_noti.enum.js'
 
 export const createAdminController = async (req: Request, res: Response, next: NextFunction) => {
   const { full_name, email, password } = req.body
   const admin = await adminService.createAdmin(full_name, email, password)
+  await notificationService.createNotification(
+    {
+      type: TypeNoti.ADMIN_CREATED_SUCCESS,
+      title: 'Admin created successfully',
+      message: 'Your admin has been created successfully'
+    },
+    admin.account_id
+  )
   res.status(201).json({
     message: ADMIN_MESSAGES.ADMIN_CREATED_SUCCESS,
     data: admin
@@ -14,6 +24,14 @@ export const createAdminController = async (req: Request, res: Response, next: N
 export const createManagerController = async (req: Request, res: Response, next: NextFunction) => {
   const { full_name, email, password } = req.body
   const manager = await adminService.createManager(full_name, email, password)
+  await notificationService.createNotification(
+    {
+      type: TypeNoti.MANAGER_CREATED_SUCCESS,
+      title: 'Manager created successfully',
+      message: 'Your manager has been created successfully'
+    },
+    manager.account_id
+  )
   res.status(201).json({
     message: ADMIN_MESSAGES.MANAGER_CREATED_SUCCESS,
     data: manager
@@ -23,6 +41,14 @@ export const createManagerController = async (req: Request, res: Response, next:
 export const createStaffController = async (req: Request, res: Response, next: NextFunction) => {
   const { full_name, email, password } = req.body
   const staff = await adminService.createStaff(full_name, email, password)
+  await notificationService.createNotification(
+    {
+      type: TypeNoti.STAFF_CREATED_SUCCESS,
+      title: 'Staff created successfully',
+      message: 'Your staff has been created successfully'
+    },
+    staff.account_id
+  )
   res.status(201).json({
     message: ADMIN_MESSAGES.STAFF_CREATED_SUCCESS,
     data: staff
@@ -40,6 +66,14 @@ export const getAllStaffController = async (req: Request, res: Response, next: N
 export const createConsultantController = async (req: Request, res: Response, next: NextFunction) => {
   const { full_name, email, password } = req.body
   const consultant = await adminService.createConsultant(full_name, email, password)
+  await notificationService.createNotification(
+    {
+      type: TypeNoti.CONSULTANT_CREATED_SUCCESS,
+      title: 'Consultant created successfully',
+      message: 'Your consultant has been created successfully'
+    },
+    consultant.account_id
+  )
   res.status(201).json({
     message: ADMIN_MESSAGES.CONSULTANT_CREATED_SUCCESS,
     data: consultant
@@ -49,6 +83,14 @@ export const createConsultantController = async (req: Request, res: Response, ne
 export const createCustomerController = async (req: Request, res: Response, next: NextFunction) => {
   const { full_name, email, password } = req.body
   const customer = await adminService.createCustomer(full_name, email, password)
+  await notificationService.createNotification(
+    {
+      type: TypeNoti.CUSTOMER_CREATED_SUCCESS,
+      title: 'Customer created successfully',
+      message: 'Your customer has been created successfully'
+    },
+    customer.account_id
+  )
   res.status(201).json({
     message: ADMIN_MESSAGES.CUSTOMER_CREATED_SUCCESS,
     data: customer
